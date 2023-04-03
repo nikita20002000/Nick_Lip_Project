dfbae8c33ddb9dd00d364c7afc4c17ae5e07e19b
nikita20002000 <vertihost@yandex.com>
2023-04-03 22:59:27 +0300

    ticket3: Добавил реализацию вкладки меню "О программе", реализовал логику

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-# Это основной файл и в нем будет реализовано окно программы с помощью библиотеки PyQT6
+# Это основной файл и в нем будет реализовано окно программы с помощью библиотеки PyQT5
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Window.mainW import Ui_MainWindow
 from Window.developer_info import Ui_Dialog
+from Window.about_programm import Ui_Dialog_1
+
 
 
 def create_description():
@@ -26,6 +28,12 @@ class developers_window(QtWidgets.QDialog, Ui_Dialog):
         self.setupUi(self)
         self.setWindowIcon(QtGui.QIcon())
 
+class about_programm(QtWidgets.QDialog, Ui_Dialog_1):
+    def __init__(self, parent=None):
+        super(about_programm, self).__init__()
+        self.setupUi_1(self)
+        self.setWindowIcon(QtGui.QIcon())
+
 
 class Main(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
@@ -42,10 +50,21 @@ class Main(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setWindowFlags(QtCore.Qt.WindowMaximizeButtonHint)
 
         self.action.triggered.connect(self.show_developer_info)  ##подключил кнопку вывода инфы про разработчиков
+        self.actionEduHelper.triggered.connect(self.show_about_programm)  ##подключ
 
     def show_developer_info(self):
         self.Dialog = developers_window()
         self.Dialog.show()
+
+    def show_about_programm(self):
+        self.Dialog_1 = about_programm()
+        self.Dialog_1.show()
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
